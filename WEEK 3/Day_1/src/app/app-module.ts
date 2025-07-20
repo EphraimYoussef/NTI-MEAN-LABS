ed39e188dad6817db4dd22f93bbaf3281dab5482
Ephraim Youssef <ephraim.youssf@gmail.com>
2025-07-20 23:39:35 +0300

    Week 3 - Day 1

---
+++ WEEK 3/Day_1/src/app/app-module.ts
@@ -0,0 +1,24 @@
+import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing-module';
+import { App } from './app';
+import { Navbar } from './components/navbar/navbar';
+import { Posts } from './components/posts/posts';
+
+@NgModule({
+  declarations: [
+    App,
+    Navbar,
+    Posts
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideBrowserGlobalErrorListeners()
+  ],
+  bootstrap: [App]
+})
+export class AppModule { }
---
+++ WEEK 3/Day_1/src/app/interfaces/post.interface.ts
@@ -0,0 +1,7 @@
+export interface Ipost {
+    id:number;
+    userId: number;
+    username: string;
+    title: string;
+    body: string;
+}
